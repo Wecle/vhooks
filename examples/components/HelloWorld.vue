17ec47f6d2bf98d3be7a30707cf4fc3ddf51e96f
郑远龙 <974773303@qq.com>
2024-02-29 00:14:08 +0900

    VHOOKS-240229 fix(*)：App.vue引入问题

--- examples/components/HelloWorld.vue
+++ examples/components/HelloWorld.vue
@@ -10,12 +10,10 @@
 </template>
 
 <script setup lang="ts">
-import useToggle from '../../src/useToggle'
+import { useToggle } from '../../src'
 // import { useToggle } from '../../dist/vhooks.umd'
 
 console.log(useToggle)
 
 const [state, { toggle, setLeft, setRight }] = useToggle()
 </script>
-
-
